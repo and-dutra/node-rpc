7bfaf238c8aec0b973dcafc5701325b4ed6cac38
André D Oliveira <decaobr@gmail.com>
2018-09-20 21:19:24 -0300

    Soma e Subtracao adicionados

--- client/node/index.js
+++ client/node/index.js
@@ -1,52 +1,21 @@
 const grpc = require('grpc');
 
 const protoPath = require('path').join(__dirname, '../..', 'proto');
-const proto = grpc.load({root: protoPath, file: 'employees.proto'});
-const client = new proto.employees.EmployeesService('localhost:50050', grpc.credentials.createInsecure());
+const proto = grpc.load({root: protoPath, file: 'calculadora.proto'});
+const client = new proto.calculadora.CalculadoraService('localhost:50050', grpc.credentials.createInsecure());
 
-client.List({}, (error, response) => {
+client.Soma({val: 10, val2: 30}, (error, response) => {
    if (!error) {
-      console.log("Response : ", response)
+      console.log("Resultado Soma : ", response.message);
    }
    else {
       console.log("Error:", error.message);
    }
 });
 
-client.get({
-   employee_id: 860294//parseInt(Math.random() * 1000000)
-}, (error, response) => {
-   console.log("error, response : ", error, response)
+client.Subtrai({val: 344, val2: 30}, (error, response) => {
    if (!error) {
-      console.log("Response : ", response)
-   }
-   else {
-      console.log("Error:", error.message);
-   }
-});
-
-/*client.remove({
-   employee_id: 178646//parseInt(Math.random() * 1000000)
-}, (error, response) => {
-   if (
-      !error
-   ) {
-      console.log("Response : ", response)
-   }
-   else {
-      console.log("Error:", error.message);
-   }
-});
-*/
-client.Insert({
-   employee_id: parseInt(Math.random() * 1000000),
-   name: "Andre Dutra",
-   email: "andredutra@univem.edu.br"
-}, (error, response) => {
-   if (
-      !error
-   ) {
-      console.log("Response : ", response)
+      console.log("Resultado Subtracao : ", response.message);
    }
    else {
       console.log("Error:", error.message);
--- server/index.js
+++ server/index.js
@@ -1,49 +1,17 @@
 const grpc = require('grpc');
-global.Mongoose = require('mongoose');
-Mongoose.connect('mongodb://grpc:grpc123@ds211143.mlab.com:11143/grpc');
-
-const proto = grpc.load('proto/employees.proto');
+const proto = grpc.load('proto/calculadora.proto');
 const server = new grpc.Server();
-const employeeServices = require('../db/employees');
+//const employeeServices = require('../db/employees');
 
-server.addService(proto.employees.EmployeesService.service, {
-
-   List(call, callback){
-      employeeServices.list(callback);
-   },
-
-   get(call, callback){
-      let payload = {
-         criteria: {
-            employee_id: call.request.employee_id
-         },
-         projections: {
-            _id: 0, __v: 0
-         },
-         options: {
-            lean: true
-         }
-      };
-      let emp = new employeeServices(payload);
-      emp.fetch(callback);
-   },
-
-   Insert(call, callback){
-      let emp = new employeeServices({
-         employee_id: call.request.employee_id,
-         name: call.request.name,
-         email: call.request.email,
-      });
-      emp.add(callback);
-   },
-
-   remove(call, callback){
-      const criteria = {
-         employee_id: call.request.employee_id,
-      };
-      let emp = new employeeServices(criteria);
-      emp.remove(criteria, callback);
-   },
+server.addService(proto.calculadora.CalculadoraService.service, {
+    Soma(call, callback) {
+        let resultado = call.request.val + call.request.val2;
+        callback(null, {message: resultado});
+    }, 
+    Subtrai(call, callback) {
+        let resultado = call.request.val - call.request.val2;
+        callback(null, {message: resultado});
+    }, 
 });
 
 
